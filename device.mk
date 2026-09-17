a40f0aae32f349be5b033ff1ccc0e112dafa0583
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-18 01:49:29 +0600

    sweet: Add MiuiCamera product inheritance in device.mk

--- device.mk
+++ device.mk
@@ -100,3 +100,6 @@ PRODUCT_PACKAGES += \
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
+
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/miuicamera-sweet/device.mk)
