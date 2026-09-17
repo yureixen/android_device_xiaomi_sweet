4a5db9ebe379cd4d08aa4204ea7127ee9d54a61f
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-18 01:47:51 +0600

    sweet: MiuiCamera configuration inclusion
    Comment out the inclusion of MiuiCamera BoardConfig.mk.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,3 +42,6 @@ VENDOR_SECURITY_PATCH := 2023-11-01
 
 # Inherit from proprietary files
 include vendor/xiaomi/sweet/BoardConfigVendor.mk
+
+# MiuiCamera
+-include vendor/miuicamera-sweet/BoardConfig.mk
